e0cb541565ec30c0bb7bbc06567e2aed8a72d846
Zac McCormick <zac.mccormick@gmail.com>
2012-11-19 01:23:38 -0500

    added more cuisines

--- tags.rb
+++ tags.rb
@@ -1,9 +1,11 @@
 require 'json'
 
-tags = JSON.parse(`curl -sq "http://taginfo.openstreetmap.org/api/2/db/keys/values?key=craft\&sortname=count_all\&rp=500\&sortorder=desc\&page=1"`)
+tag_name = 'cuisine'
+
+tags = JSON.parse(`curl -sq "http://taginfo.openstreetmap.org/api/2/db/keys/values?key=#{tag_name}\&sortname=count_all\&rp=500\&sortorder=desc\&page=1"`)
 
 current_form = File.read('./models/classifications.json')
 
 tags['data'].each_with_index do |t, i|
-  puts "#{t['value']}, #{t['count']}, #{t['description']}" unless current_form.include? "craft=#{t['value']}"
+  puts "#{t['value']}, #{t['count']}, #{t['description']}" unless current_form.include? "#{tag_name}=#{t['value']}"
 end
